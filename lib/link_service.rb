0f453b163a8f197b6ca2279028100d2a414649c5
Georg Apitz <georg@usebar.org>
2013-12-06 00:01:31 -0700

    get movie page url, parse page and get download url

---
+++ lib/link_service.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+
+class LinkService
+  def link_for_url(url)
+    link = nil
+    open(url) do |f|
+      f.each_line do |line|
+        link = line.scan(/^\s+mediaCollection\.addMediaStream\(1,\s2,\s"",\s"(.*)",\s"default"\);/)
+        break if link.length > 0
+      end
+    end
+    link.flatten[0] unless link.length == 0
+  end
+end
--- lib/schimanski.rb
+++ lib/schimanski.rb
@@ -1,18 +1,23 @@
 require "sinatra"
 require "net/http"
 require "json"
+require_relative "link_service"
 
 class Schimanski < Sinatra::Base
   get "/health" do
     "Schimanski is ready to solve the next case"
   end
 
-  get "/" do
-    <<-HTML
-<center>
-<h2>Get download urls here</h2>
+  post "/" do
+    movie_url = params[:url]
+    link_service = LinkService.new
+    link = link_service.link_for_url(movie_url)
+    p link
+    p movie_url
+    haml :index, locals: { link: link, movie_url: movie_url }
+  end
 
-</center>
-    HTML
+  get "/" do
+    haml :new
   end
 end
