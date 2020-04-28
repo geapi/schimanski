8a699ae37d12451812005a1a95268fc9a6359c34
Georg Apitz <georg@usebar.org>
2020-04-28 14:57:39 +0200

    add link to mediathekviewweb for solution

--- lib/link_service.rb
+++ lib/link_service.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class LinkService
   def link_for_url(url)
     link = nil
-    open(url) do |f|
+    URI.open(url) do |f|
       f.each_line do |line|
         link = line.scan(/^\s+mediaCollection\.addMediaStream\(1,\s2,\s"",\s"(.*)",\s"default"\);/)
         break if link.length > 0
